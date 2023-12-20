d698b3e50167427f2d5848a0f47f93e197171261
John Henry Rudden <johnhenryrudden@gmail.com>
2023-12-20 17:32:04 -0600

    Update simple RNN to include trainable parameters for output calculation V

--- rnn/simple_rnn/simple_rnn.py
+++ rnn/simple_rnn/simple_rnn.py
@@ -1,5 +1,5 @@
 # Simple Recurrent Neural Network Implementation
-# Reference:
+# Reference (conceptual)):
 # Jurafsky, D., & Martin, J.H. (2022). "Recurrent Neural Networks." In Speech and Language Processing (3rd ed.). 
 # Retrieved from https://web.stanford.edu/~jurafsky/slp3/9.pdf
 
@@ -13,7 +13,9 @@ class SimpleRNN(nn.Module):
         self.output_dim = output_dim
         self.U = nn.Parameter(torch.rand(hidden_dim, hidden_dim))
         self.W = nn.Parameter(torch.rand(hidden_dim, input_dim))
-        self.hidden_layer = nn.Linear(hidden_dim, output_dim)
+        self.V = nn.Parameter(torch.rand(output_dim, hidden_dim))
+
+        self.hidden_state = torch.zeros((hidden_dim, 1))
 
         # mental model of hidden layer products
         # input_dim = n
@@ -21,16 +23,21 @@ class SimpleRNN(nn.Module):
         # U @ h_t-1 = m x m @ m x 1 = m x 1
         # W @ x = m x n @ n x 1 = m x 1
         # => m x 1 + m x 1 = m x 1
-        # so hidden layer output is m x 1 (same as hidden_dim) check!
+        # so hidden state output is m x 1 (same as hidden_dim) check!
     
-    def forward(self, x, h_t_minus_1):
-        hidden_contribution = self.U @ h_t_minus_1
+    def step(self, x):
+        # calculate contribution from previous hidden state
+        hidden_contribution = self.U @ self.hidden_state
         input_contribution = self.W @ x
         hidden_input = hidden_contribution + input_contribution
-        logits = self.hidden_layer(hidden_input)
 
-        return logits, hidden_input
+        # calculate new hidden state
+        self.hidden_state = torch.tanh(hidden_input)
 
-    def get_initial_prev_hidden_activation(self):
+        # calculate output
+        logits = self.V @ self.hidden_state
+        return logits
+
+    def get_initial_prev_activation(self):
         return torch.zeros((self.hidden_dim, 1))
         
