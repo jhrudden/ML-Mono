ff90561ba7db885c122a183e145b661a66591b45
John Henry Rudden <johnhenryrudden@gmail.com>
2023-12-25 19:03:35 -0600

    generalize utility for fetching datasets

--- notebooks/datasets/utility.py
+++ notebooks/datasets/utility.py
@@ -1,26 +1,30 @@
 import tensorflow_datasets as tfds
 
-def load_dataset(dataset_name: str, split_name: str = 'train'):
+def load_dataset(dataset_name: str, split_name: str = 'train', text_key: str = 'text'):
     """
     Load lines from a specified TensorFlow dataset and split.
 
     Parameters:
     - dataset_name (str): Name of the dataset to load.
     - split_name (str): Name of the split to load (default is 'train').
+    - text_key (str): Key in the dataset that contains the text (default is 'text').
 
     Returns:
-    - List[str]: A list of lines extracted from the dataset.
+    - str: All lines from the specified dataset and split concatenated into a single string.
     """
 
-    # Load the specified dataset and split
-    dataset = tfds.load(dataset_name, split=split_name)
+    try:
+        # Load the specified dataset and split
+        dataset = tfds.load(dataset_name, split=split_name, as_supervised=False)
+    except Exception as e:
+        raise RuntimeError(f"Failed to load dataset '{dataset_name}' with split '{split_name}': {e}")
 
-    # Extract lines
-    lines = []
+    data = []
     for example in dataset:
-        text = example['text'].numpy().decode('utf-8')
-        current_lines = text.split('\n')
-        current_lines = list(filter(lambda line: len(line) > 0, current_lines))
-        lines.extend(current_lines)
+        try:
+            text = example[text_key].numpy().decode('utf-8')
+            data.append(text)
+        except KeyError:
+            raise KeyError(f"Key '{text_key}' not found in the dataset '{dataset_name}'. Please specify the correct key.")
 
-    return lines
+    return "\n".join(data)  # Joining with newline character, can be adjusted as needed
