5072d544fb050dc1b4b6f7f0ae884ab53a4a2dd2
John Henry Rudden <johnhenryrudden@gmail.com>
2023-12-25 19:13:10 -0600

    pre batch implementation (wip)

--- rnn/simple_rnn/simple_rnn.py
+++ rnn/simple_rnn/simple_rnn.py
@@ -3,6 +3,7 @@
 # Jurafsky, D., & Martin, J.H. (2022). "Recurrent Neural Networks." In Speech and Language Processing (3rd ed.). 
 # Retrieved from https://web.stanford.edu/~jurafsky/slp3/9.pdf
 
+import torch
 from torch import nn
 
 class SimpleRNN(nn.Module):
@@ -11,33 +12,83 @@ class SimpleRNN(nn.Module):
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
         self.output_dim = output_dim
-        self.U = nn.Parameter(torch.rand(hidden_dim, hidden_dim))
-        self.W = nn.Parameter(torch.rand(hidden_dim, input_dim))
-        self.V = nn.Parameter(torch.rand(output_dim, hidden_dim))
+        self.U = nn.Parameter(torch.randn(hidden_dim, hidden_dim))
+        self.W = nn.Parameter(torch.randn(hidden_dim, input_dim))
+        self.V = nn.Parameter(torch.randn(output_dim, hidden_dim))
+        self.reset_hidden_state()
 
-        self.hidden_state = torch.zeros((hidden_dim, 1))
 
-        # mental model of hidden layer products
-        # input_dim = n
-        # hidden_dim = m
-        # U @ h_t-1 = m x m @ m x 1 = m x 1
-        # W @ x = m x n @ n x 1 = m x 1
-        # => m x 1 + m x 1 = m x 1
-        # so hidden state output is m x 1 (same as hidden_dim) check!
     
-    def step(self, x):
+    def step(self, X):
+        if X.dim == 1:
+            X = X.view(1, -1)
+        print(f'hidden state size: ', self.hidden_state.size())
         # calculate contribution from previous hidden state
         hidden_contribution = self.U @ self.hidden_state
-        input_contribution = self.W @ x
+        input_contribution = self.W @ X.T
         hidden_input = hidden_contribution + input_contribution
+        print(f'sizes of hidden_contribution, input_contribution, hidden_input: ', hidden_contribution.size(), input_contribution.size(), hidden_input.size())
 
         # calculate new hidden state
         self.hidden_state = torch.tanh(hidden_input)
 
         # calculate output
         logits = self.V @ self.hidden_state
-        return logits
+        return logits.T # transpose to match batch size
 
-    def get_initial_prev_activation(self):
-        return torch.zeros((self.hidden_dim, 1))
+    def reset_hidden_state(self):
+        self.hidden_state = torch.zeros((self.hidden_dim, 1))
+
+class LanguageModelSRNN(nn.Module):
+    """
+    Language model implemented with a simple RNN.
+    """
+    def __init__(self, vocab_size: int, seq_len: int, hidden_dim: int):
+        super(LanguageModelSRNN, self).__init__()
+        self.rnn = SimpleRNN(vocab_size, hidden_dim, vocab_size)
+    
+    def forward(self, x):
+        """
+        Forward pass of the model.
+        :param x: input tensor of shape (batch_size, seq_len)
+        :return: output tensor of shape (batch_size, seq_len, vocab_size)
+        """
+        # one hot encode input
+        x = self.embedding(x)
+        outputs = []
+        self.rnn.reset_hidden_state()
+        for i in range(x.shape[1]):
+            logits = self.rnn.step(embedding[:, i, :])
+            softmaxed = torch.softmax(logits, dim=1)
+            outputs.append(softmaxed)
+        
+        return torch.stack(outputs, dim=1)
+    
+    @torch.no_grad()
+    def generate(self, context, seq_len: int, num_samples: int = 1):
+        """
+        Generate a sequence of text using the model.
+        :param seq_len: the length of the sequence to generate
+        :return: a sequence of text
+        """
+        self.eval()
+        self.rnn.reset_hidden_state()
+        for i in range(num_samples):
+            current_context = context[:min(seq_len, len(context))]
+            current_context = self.embedding(current_context.view(1, -1))
+            for char in current_context[0]:
+                logits = self.rnn.step(char.T)
+            softmaxed = torch.softmax(logits, dim=1)
+            next_char = torch.multinomial(softmaxed, num_samples=1).view(-1)
+            context = torch.cat([context, next_char], dim=0)
+        self.train()
+        return context.T.detach().tolist()
+    
+    def embedding(self, x):
+        """
+        Embedding layer for the model.
+        :param x: input tensor of shape (batch_size, seq_len)
+        :return: output tensor of shape (batch_size, seq_len, vocab_size)
+        """
+        return torch.nn.functional.one_hot(x, num_classes=self.rnn.input_dim).float()
         
