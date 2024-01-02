e47672cc766a716c9357f524ffc93d9d0fa7f8d7
John Henry Rudden <johnhenryrudden@gmail.com>
2024-01-01 19:38:20 -0600

    Update defaults

--- supervised/logistic_regression.py
+++ supervised/logistic_regression.py
@@ -8,7 +8,7 @@ class LogisticRegression:
     def _sigmoid(self, z):
         return 1 / (1 + np.exp(-z))
 
-    def fit(self, X, y, num_iterations: int = 1_000, lr=1e-3): # notice no closed form solution as first order derivative is none linear
+    def fit(self, X, y, num_iterations: int = 1_000, lr=1e-1): # notice no closed form solution as first order derivative is none linear
         X = np.hstack((np.ones((X.shape[0], 1)), X))
         self.weights = np.random.randn(X.shape[1])
         for _ in range(num_iterations):
@@ -16,9 +16,15 @@ class LogisticRegression:
             # (Notice: looks like linear regression)
             self.weights -= lr * dw
         
-    def predict(self, X):
+    def predict(self, X, as_prob: bool = False):
         X = np.hstack((np.ones((X.shape[0], 1)), X))
-        return self._sigmoid(X @ self.weights)
+        y_hat = self._sigmoid(X @ self.weights)
+        if as_prob:
+            return y_hat
+        return np.round(y_hat)
+    
+    def predict_proba(self, X):
+        return self.predict(X, as_prob=True)
 
 
     
