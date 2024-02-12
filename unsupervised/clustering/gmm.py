9662db664b425ca6b300ece42bf093e81bcdfbb9
John Henry Rudden <johnhenryrudden@gmail.com>
2024-02-12 14:05:44 -0500

    update GMM to use BaseClusterMixin

--- unsupervised/clustering/gmm.py
+++ unsupervised/clustering/gmm.py
@@ -1,11 +1,11 @@
 # trying EM
 from scipy.stats import multivariate_normal
 import numpy as np
+from .base import BaseClusterMixin
 
-class GaussianMixture:
-    def __init__(self, n_components=2, max_iter=100):
-        self.n_components = n_components
-        self.max_iter = max_iter
+class GaussianMixture(BaseClusterMixin):
+    def __init__(self, n_clusters=2, max_iter=100):
+        super().__init__(n_clusters=n_clusters, max_iter=max_iter)
         self.weight_matrix = None
         self.phis = None
         self.means = None
@@ -19,10 +19,10 @@ class GaussianMixture:
             X: (n_samples, n_features) array
         """
         n_samples, n_features = X.shape
-        self.phis = np.ones(self.n_components) / self.n_components
-        self.weight_matrix = np.ones((n_samples, self.n_components)) / self.n_components
-        self.means = np.random.choice(X.flatten(), size=(self.n_components, n_features))
-        self.covs = np.array([np.cov(X, rowvar=False) for _ in range(self.n_components)])
+        self.phis = np.ones(self.n_clusters) / self.n_clusters
+        self.weight_matrix = np.ones((n_samples, self.n_clusters)) / self.n_clusters
+        self.means = np.random.choice(X.flatten(), size=(self.n_clusters, n_features))
+        self.covs = np.array([np.cov(X, rowvar=False) for _ in range(self.n_clusters)])
 
     def _e_step(self, X):
         """
@@ -34,8 +34,8 @@ class GaussianMixture:
             X: (n_samples, n_features) array
         """
         n_samples = X.shape[0]
-        responsibilities = np.zeros((n_samples, self.n_components))
-        for i in range(self.n_components):
+        responsibilities = np.zeros((n_samples, self.n_clusters))
+        for i in range(self.n_clusters):
             responsibilities[:, i] = multivariate_normal.pdf(X, mean=self.means[i], cov=self.covs[i])
         sum_responsibilities = responsibilities.dot(self.phis)
         return responsibilities / sum_responsibilities[:, np.newaxis]
@@ -49,7 +49,7 @@ class GaussianMixture:
         Params:
             X: (n_samples, n_features) array
         """
-        for i in range(self.n_components):
+        for i in range(self.n_clusters):
             weights = self.weight_matrix[:, i]
             total_weight = weights.sum()
             self.means[i] = np.sum(X * weights[:, np.newaxis], axis=0) / total_weight
@@ -65,10 +65,17 @@ class GaussianMixture:
             X: (n_samples, n_features) array
         """
         self._init(X)
+        prev_log_likelihood = -np.infty
         for _ in range(self.max_iter):
             self.weight_matrix = self._e_step(X)
             self.phis = self.weight_matrix.mean(axis=0)
             self._m_step(X)
+            
+            # Check for convergence
+            log_likelihood = self._log_likelihood(X)
+            if log_likelihood - prev_log_likelihood < self.tol:
+                break
+            prev_log_likelihood = log_likelihood
 
     def predict(self, X):
         """
@@ -82,20 +89,24 @@ class GaussianMixture:
         self.weight_matrix = self._e_step(X)
         return np.argmax(self.weight_matrix, axis=1)
     
-    def fit_predict(self, X):
+    def _log_likelihood(self, X):
         """
-        Fit the model to the data and predict the cluster labels
+        Calculate the log likelihood of the data given the model.
         Params:
             X: (n_samples, n_features) array
         Returns:
-            labels: (n_samples,) array of cluster labels
+            mle: float, log likelihood of the data
         """
-        self.fit(X)
-        return self.predict(X)
-
+        m, _ = X.shape
+        probs = np.zeros((m, self.n_clusters))
+        for i in range(self.n_clusters):
+            probs[:, i] = self.phis[i] * multivariate_normal.pdf(X, mean=self.means[i], cov=self.covs[i])
+        
+        return np.sum(np.log(probs.sum(axis=1)))
+        
     def pdf(self, X):
         """Calculate the probability density function for each sample in X."""
         pdf_values = np.zeros(X.shape[0])
-        for i in range(self.n_components):
+        for i in range(self.n_clusters):
             pdf_values += self.phis[i] * multivariate_normal.pdf(X, mean=self.means[i], cov=self.covs[i])
         return pdf_values
